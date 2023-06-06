77c6d5244a4f3c5ee196912b380a4671fd8883cc
kalilsub <kalil.subaanburaaleh@uzh.ch>
2023-06-06 19:59:22 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
 
       <ReactLogoCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-1 w-full flex justify-center items-center lg:hidden">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
